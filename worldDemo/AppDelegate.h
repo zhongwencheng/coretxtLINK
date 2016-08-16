66e7673f9f0aca9df8a3b3f59f0167f69bca621e
(外包) 钟文成 <ex-zhongwencheng001@zhongwenheng.local>
2016-08-16 14:22:58 +0800

    Initial Commit

---
+++ worldDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  worldDemo
+//
+//  Created by 钟文成(外包) on 16/8/16.
+//  Copyright © 2016年 钟文成(外包). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
