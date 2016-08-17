ce7d601f0d4caf1777a5a85da3f021b0b043e202
钟文成(外包) <ex-zhongwencheng001@zhongwenheng.local>
2016-08-17 17:52:08 +0800

    first commit

---
+++ worldDemo/DisplayView.h
@@ -0,0 +1,13 @@
+//
+//  DisplayView.h
+//  worldDemo
+//
+//  Created by 钟文成(外包) on 16/8/16.
+//  Copyright © 2016年 钟文成(外包). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DisplayView : UIView
+
+@end
---
+++ worldDemo/DisplayView.m
@@ -0,0 +1,21 @@
+//
+//  DisplayView.m
+//  worldDemo
+//
+//  Created by 钟文成(外包) on 16/8/16.
+//  Copyright © 2016年 钟文成(外包). All rights reserved.
+//
+
+#import "DisplayView.h"
+
+@implementation DisplayView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
